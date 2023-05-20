1172609f6e65affc8fb65afc14b81976a9236011
Shamimusman515419 <shamimhossain01617@gmail.com>
2023-05-20 21:47:19 +0600

    delete toy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
+  // try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect((err)=>{
     //    if(err){
@@ -136,10 +136,10 @@ app.get("/sorting/:id", async(req,res)=>{
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+  // } finally {
+  //   // Ensures that the client will close when you finish/error
+  //   // await client.close();
+  // }
 }
 run().catch(console.dir);
 
